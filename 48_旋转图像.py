aa3baca3716747df96b0d7663bee5e444d6d8bdf
jingminhao <jingminhao666@gmail.com>
2020-12-20 00:08:33 +0800

    12.20

--- 48_旋转图像.py
+++ 48_旋转图像.py
@@ -1,8 +1,16 @@
+from typing import List
+
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
+        if n<=1:
+            return
         for i in range(n):
             for j in range(i,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
         for i in range(n):
-            matrix[i] = matrix[::-1]
+            matrix[i] = matrix[i][::-1]
+
+sol = Solution()
+ans = sol.rotate([[]])
+print(ans)
